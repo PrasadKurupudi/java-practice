3301127e3e4bf6a4b80a952892dbf3cc7aa62b9c
Kurupudi Durga Veera Prasad <durgaprasad7051@gmail.com>
2025-08-23 20:29:22 +0530

    java practice
    java basics

---
+++ demo.java
@@ -0,0 +1,138 @@
+public class demo {
+    public static void main(String[] args){
+        System.out.println("malli modalu pedta scarch nunchiii");
+       /* Byte a = 25;
+        Short b = 255;
+        int d = 1000;
+        long c = 100000000;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        
+        boolean b = true;
+        char c = 'p';
+        float d = 1.25896f;
+        int e = 55;
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(Integer.MIN_VALUE);
+
+        int a = 10;
+        float f = a;
+        float g = 10.2255f;
+        int h = (int)g;
+        System.out.println(f);
+        System.out.println(a);
+        System.out.println(h);
+        System.out.println(g);
+
+        
+        int a = 5;
+        System.out.println(a += 5);
+        System.out.println(a -= 2);
+        System.out.println(a *= 3);
+        System.out.println(a /= 2);
+        System.out.println(a %= 5);
+
+        
+
+        int a = 10;
+        int b = 5;
+        int c = 5;
+
+        System.out.println(a == b );
+        System.out.println(b != c );
+        System.out.println(c < a );
+        System.out.println(a <=c );
+        System.out.println(b> c );
+        System.out.println(c >= a);
+
+        
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1 && b2); // false
+        System.out.println(b1||b2);
+        System.out.println(!b1 + " " + !b2);
+
+        
+
+        int a = 10;
+        int b = 10;
+        System.out.println(a++); 
+        System.out.println(++a);
+        System.out.println(b--); 
+        System.out.println(--b);
+
+        
+
+        int a = 10 , b = 5 , min;
+        min = (a < b) ? a : b ;
+        System.out.println(min);
+
+        */
+
+        // int a = 30;
+        // int b = 11;
+        // if(a + b > 21){                                            ------ if else 
+        //     System.out.println("The sum is greater than 21");
+        // }else{
+        //     System.out.println("The sum is lesser then 21");
+        // }
+        // if(a + b == 21){                                            ----- if else if
+        //     System.out.println("the sum is equal to 21");
+        // }else if(a+b > 21){
+        //     System.out.println("The sum is grater then 21");
+        // }else if(a+b < 21){
+        //     System.out.println("the sum is lesser then 21");
+        // }
+        // if(a+b < 50){                                               ------ nested if
+        //     System.out.println("its lesser then 50");
+        //     if(a+b<30){
+        //         System.out.println("its lesser then 30");
+        //     }else{
+        //         System.out.println("its grater then 30");
+        //     }
+        // }else{
+        //     System.out.println("its grater then 50");
+        // }
+
+        // int num = 30; 
+        // switch (num) {
+        //     case 10:
+        //         System.out.println("hello");
+        //         break;
+        //     case 20:
+        //         System.out.println("hello1");
+        //         break;
+        //     case 30:
+        //         System.out.println("hello2");
+        //         break;
+        
+        //     default:
+        //     System.out.println("hello3");
+        //         break;
+        // }
+
+        // String a = "prasad";
+        // for(int i=0; i<=10; i++){
+            
+        //     if(i==10){
+        //         System.out.println("batman");
+        //     }else{
+        //         System.out.println(a);
+        //     }
+        // }
+
+        int a = 0;
+        do{
+            a++;
+            System.out.println(a);
+        }while (a==5);
+          
+    }
+}
+   
